1841be10878f154b96463e6647d5fee256631cac
CStokes81 <69875966+CStokes81@users.noreply.github.com>
2021-02-23 17:43:40 -0500

    First Commit

---
+++ index.js
@@ -0,0 +1,50 @@
+// Some random colors
+const colors = [
+  "#f542e3", //pink
+  "#42e6f5", //blue
+  "#b6f542", //neon green
+  "#f59942", //orange
+  "#cc1f33", //red
+  "#ecf542", //yellow
+  "#cc1f33", //purple
+  "#FFFFFF", //white
+];
+
+const numBalls = 50;
+const balls = [];
+
+for (let i = 0; i < numBalls; i++) {
+  let ball = document.createElement("div");
+  ball.classList.add("ball");
+  ball.style.background = colors[Math.floor(Math.random() * colors.length)];
+  ball.style.left = `${Math.floor(Math.random() * 100)}vw`;
+  ball.style.top = `${Math.floor(Math.random() * 100)}vh`;
+  ball.style.transform = `scale(${Math.random()})`;
+  ball.style.width = `${Math.random()}em`;
+  ball.style.height = ball.style.width;
+
+  balls.push(ball);
+  document.body.append(ball);
+}
+
+// Keyframes
+balls.forEach((el, i, ra) => {
+  let to = {
+    x: Math.random() * (i % 2 === 0 ? -11 : 11),
+    y: Math.random() * 12,
+  };
+
+  let anim = el.animate(
+    [
+      { transform: "translate(0, 0)" },
+      { transform: `translate(${to.x}rem, ${to.y}rem)` },
+    ],
+    {
+      duration: (Math.random() + 1) * 2000, // random duration
+      direction: "alternate",
+      fill: "both",
+      iterations: Infinity,
+      easing: "ease-in-out",
+    }
+  );
+});
